9d4e8e89a8ac051787ebefd571e8d726e837c1e0
shimomo <yuichi@shimomo.net>
2016-07-17 01:31:37 +0900

    update #4 and #5

--- public/javascripts/submit.js
+++ public/javascripts/submit.js
@@ -1,26 +1,17 @@
 $(function() {
   $('body').on("click keydown", function(e) {
-    // Enterキー以外の場合は対象外
     if (e.keyCode !== 13) { return; }
 
-    // フォーカスされている要素のidがmessage以外の場合は対象外
     var id = $(':focus').attr('id');
     if (id !== 'message') { return; }
 
-    // messageが空文字列の場合は対象外
     var message = $('#' + id).val();
     if (message === '') { return; }
 
-    // 追加
     $('#chat-frame').append('<p class="chat-talk"><span class="talk-icon"><img src="./images/me.png" alt="me" class="icon"></span><span class="talk-content grey lighten-3 left-align">' + message + '</span></p>');
-
-    // スクロール
     $('#chat-frame').animate({scrollTop: $('#chat-frame')[0].scrollHeight}, 'fast');
-
-    // 初期化
     $('#' + id).val('');
 
-    // Tweet
     $.ajax({
       url: '/twitters/tweets',
       type: 'POST',
@@ -30,27 +21,27 @@ $(function() {
       dataType: 'html'
     })
     .done(function(data, textStatus, jqXHR) {
-
-
-
-      $.ajax({
-        url: '/twitters/tweets',
-        type: 'GET',
-        data: {
-          id: data.id_str
-          // id: '754180908731604992'
-        },
-        dataType: 'html'
-      })
-      .done(function(data2, textStatus, jqXHR) {
-        alert(data2);
-      })
-      .fail(function(jqXHR, textStatus, errorThrown) {
-        alert("fail");
-      });
-
-
-
+      var d = JSON.parse(data);
+      setTimeout(function() {
+        $.ajax({
+          url: '/twitters/tweets',
+          type: 'GET',
+          data: {
+            id: d['id_str']
+          },
+          dataType: 'html'
+        })
+        .done(function(data2, textStatus, jqXHR) {
+          var d2 = JSON.parse(data2);
+          var m = d2['text'];
+          m = m.replace( /@rinna_voice /g , "");
+          $('#chat-frame').append('<p class="chat-talk rinna"><span class="talk-icon"><img src="./images/rinna.jpg" alt="rinna" class="icon"></span><span class="talk-content green accent-1 right-align">' + m + '</span></p>');
+          $('#chat-frame').animate({scrollTop: $('#chat-frame')[0].scrollHeight}, 'fast');
+        })
+        .fail(function(jqXHR, textStatus, errorThrown) {
+          alert("fail");
+        });
+      }, 1000);
     })
     .fail(function(jqXHR, textStatus, errorThrown) {
       alert("fail");

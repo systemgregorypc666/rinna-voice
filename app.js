d95df3036403627a8d8dde9ebd27415c59268a86
shimomo <yuichi@shimomo.net>
2016-07-17 23:48:49 +0900

    update app.js

--- app.js
+++ app.js
@@ -4,9 +4,7 @@ var favicon      = require('serve-favicon');
 var logger       = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
-
-var routes   = require('./routes/index');
-var twitters = require('./routes/twitters');
+var routes       = require('./routes/index');
 
 var app = express();
 
@@ -23,7 +21,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/twitters', twitters);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
